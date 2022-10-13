0593b906cc9ca1536abbe7b52b03ac1b862b0625
thejahid <i.thejahid@gmail.com>
2022-10-13 22:45:16 +0600

    db added

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from "apollo-server-express";
+import express, { Application } from "express";
+
+import { connectDatabase } from "./database";
+import { typeDefs, resolvers } from "./graphql";
+
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
+
+  server.start().then(() => {
+    server.applyMiddleware({ app, path: "/api" });
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}!!`);
+  });
+
+  const listings = await db.listings.find({}).toArray();
+
+  console.log(listings);
+};
+
+mount(express());
